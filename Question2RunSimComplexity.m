75a42e92abdbe41f0e5327b8e8d154763e4b579c
David Haber <dh909@doc.ic.ac.uk>
2012-11-27 18:21:55 +0000

    Added random probability generation

--- Question2RunSimComplexity.m
+++ Question2RunSimComplexity.m
@@ -2,23 +2,24 @@ function [ complexity ] = Question2RunSimComplexity(numberOfRuns)
 %RUNSIM2 Summary of this function goes here
 %   Detailed explanation goes here
 
-P_MIN = 0.05;   % instead choose random probabilities
-P_MAX = 0.5;
-STEP = (P_MAX-P_MIN)/numberOfRuns;
-
-complexity = zeros(numberOfRuns+1, 1);
+complexity = zeros(numberOfRuns+1, 2);
 
 for r=1:numberOfRuns+1
    
-   fprintf('\nStarting run %d...\n', r); 
     
-   p = P_MIN+(r-1)*STEP;
-    
+   p = 0.05 + (rand(1) * 0.45);
+   
+   fprintf('\nStarting run %d with probability %.2f...\n',r , p); 
+   
    [ means, layer ] = Question2RunMinute( p );
    
-   complexity(r) = Question2CalcComplexity(means);
+   complexity(r, 1) = Question2CalcComplexity(means);
+   complexity(r, 2) = p;
    
-   complexity(r)
+   complexity(r, :)
+   
+   name = sprintf('run%d.mat', r);
+   save(name, complexity);
     
 end
 
